ae25b9ffb413c073307a4fd8c64a2eb3f67424ff
Mickey L <148607711+emblr@users.noreply.github.com>
2024-10-05 20:09:59 -0400

    hw

--- Homework 4/Array.java
+++ Homework 4/Array.java
@@ -1,4 +1,8 @@
 
+
+
+
+
 /*
 Take the given array and keep track of:
 1. Any duplicate numbers
@@ -7,10 +11,42 @@ Take the given array and keep track of:
 
  */
 
-public class Main {
+public class Array {
     public static void main(String[] args) {
         //Don't let the size of this scare you! no matter how big an array is, it all works the same!
         int[] myArray = {10,3,295,38,20,3,4,267,2445,10,5566,87,93,17,10,2,87,267,3176,3,82};
+        
         //you cannot use the array util. Do this one by hand :(
+        
+        for (int i = 0; i < (myArray).length; i++) {
+            for(int y = i + 1;y < (myArray).length; y++) {
+                
+                if(myArray[i] > myArray[y]){
+                    Integer holder = myArray[i];
+                    myArray[i] = myArray[y];
+                    myArray[y] = holder;
+                }
+                
+            }
+        }
+        
+
+        for (int i = 0; i < (myArray).length; i++) {
+            
+            Integer counter = 1;
+            for(int y = i + 1; y < (myArray).length;y++){
+                
+                if(myArray[i] == myArray[y]){
+                    counter += 1;
+                    myArray[y] = 0;
+                    
+                    
+                }
+                
+            }
+            if(counter != 1 && myArray[i] != 0){
+                System.out.println("Number " + myArray[i] + " appeared " + counter + " times.");
+            }
+        }
     }
 }
--- Homework 4/ArrayDebug.java
+++ Homework 4/ArrayDebug.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 //Our cheaty array helper file <3
-public class Main {
+public class ArrayDebug {
     public static void main(String[] args) {
 
         //Create me an array named "nameArray" that is a string and has 4 values in it, give me 4 names of people or animals you care about
@@ -16,9 +16,9 @@ public class Main {
         //Here's our first practice array!
         String[] sortMe = {"Apple","Kiwi","Orange","Banana","Watermelon","Strawberry"};
        //What method is missing here to actually sort our array?
-        Arrays.?(sortMe);
+        Arrays.sort(sortMe);
         //And what method is missing here to print out that array?
-        System.out.println(?);
+        System.out.println(Arrays.toString(sortMe));
         //After this, you cannot use the array helper file, gotta go back to the old ways!!
 
         //another one!
@@ -26,14 +26,25 @@ public class Main {
         //How would we add every index together and print out the sum??
         //A small hint:
         int sum = 0;
-        for(?;?;?){
-
+        for(int i = 0; i < (addMe).length;i++){
+            sum += addMe[i];
         }
+        System.out.println(sum);
         
         //thinking one! here's an array
         int maxMe[] = {280, 4892, 134, 23, 1937, 5329, 932, 89};
         //What would you have to do to find the largest number and then print that value?
-        
-
+        for (int i = 0; i < (maxMe).length; i++) {
+            for(int y = i + 1;y < (maxMe).length; y++) {
+                
+                if(maxMe[i] > maxMe[y]){
+                    Integer holder = maxMe[i];
+                    maxMe[i] = maxMe[y];
+                    maxMe[y] = holder;
+                }
+                
+            }
+        }
+        System.out.println(maxMe[(maxMe).length - 1]);
     }
 }
