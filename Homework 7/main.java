0f3db50ac4e20dba8f2f7c25c2dba86c3402ef2d
Mickey L <148607711+emblr@users.noreply.github.com>
2024-11-20 19:51:04 -0500

    hw

--- Homework 7/main.java
+++ Homework 7/main.java
@@ -2,7 +2,29 @@
 public class Main {
     public static void main(String[] args){
 
-            student MelC = new student("Melissa","Senior",3.4,1343);
+        student MelC = new student("Melissa","Senior",3.4,102);
+        student Mickey = new student("Mickey","Sophomore",4.0,105);
+        student Victor = new student("Victor","Sophomore",3.6,107);
+        student Pablo = new student("Pablo","Sophomore",3.2,101);
+        student David = new student("David","Senior",2.8,109);
+        student Nick = new student("Nick","Junior",0.0,108);
+
+
+        MelC.HonorRoll();
+        Mickey.HonorRoll();
+        Victor.HonorRoll();
+        Pablo.HonorRoll();
+        David.HonorRoll();
+        Nick.HonorRoll();
+
+        System.out.println();
+        
+        MelC.FreeLunch();
+        Mickey.FreeLunch();
+        Victor.FreeLunch();
+        Pablo.FreeLunch();
+        David.FreeLunch();
+        Nick.FreeLunch();
 
     }
 }
--- Homework 7/student.java
+++ Homework 7/student.java
@@ -7,21 +7,43 @@ check each of the students in main
 and see if any students match that id, if they do they get free lunch that day, yay!) compare a randomly generated id and the
 id of the student to print the appropriate response.
  */
+import java.util.*;
 public class student{
+    int id;
     String name;
     String year;
     double gpa;
-    int id;
+    //int id;
     public student(String n, String y, double g, int i){
         name = n;
         year = y;
         gpa = g;
         id = i;
     }
+    public static int RanNum(){
+        Random random = new Random();
+        int randInt = random.nextInt(100,110);
+        return randInt;
+    }
     
     //new method
+    void HonorRoll(){
+        if(this.gpa > 3.6){
+            System.out.println(this.name + " is an honor roll student.");
+        }else{
+            System.out.println(this.name + " is not an honor roll student.");
+        }
+    }
 
     
     //new method
+    void FreeLunch(){
+        if(this.id == RanNum()){
+            System.out.println("Congrats kid. You get to eat today for free!");
+
+        }else{
+            System.out.println("Poor you. You don't get free lunch.");
+        }
+    }
 
 }
